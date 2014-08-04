baeddcd22b275abd20d21ec6a8a16cb204e06042
Dilpreet Singh <dilpreet@vinsol.com>
2014-08-04 15:59:54 +0530

    Changing the loadcontent fucntion

--- js/specials.js
+++ js/specials.js
@@ -1,26 +1,20 @@
-function checkEvent (getElements) {
-  this.divModule = getElements.divModule;
-  this.targetDiv = "";
+function checkEvent (elements) {
+  this.divModule = elements.divModule;
 }
-checkEvent.prototype.init = function() {
-  this.targetDiv = $("<div/>").appendTo(this.divModule);
-  $(".input_submit").remove();
-  this.bindEvents();
-};
 
 checkEvent.prototype.loadContent = function(currentItem) {
   var _this = this,
-      value = currentItem.val();
-  if(!currentItem.index()) {
-    _this.targetDiv.empty();
-  }
-  else {
+      value = currentItem.val(),
+      targetDiv = $("<div/>").appendTo(this.divModule);
+  if (currentItem.index()) {
     $.ajax({
       dataType : "json",
       url : "data/specials.json",
-      cache : true,
       success : function(result,status,xhr) {
-        _this.targetDiv.html("<h2>"+result[value].title+"</h2>"+"<p>"+result[value].text+"</p>"+"<img src =" +result[value].image+"/>");
+        var title = $("<h2/>").text(result[value].title),
+            description = $("<p/>").text(result[value].text),
+            image = $("<img/>").attr("src", result[value].image);
+        targetDiv.append(title, description, image);    
       }
     });
   }
@@ -28,15 +22,17 @@ checkEvent.prototype.loadContent = function(currentItem) {
 
 checkEvent.prototype.bindEvents = function() {
   var _this = this; 
-  $("select[name=day]").find("option").on("click" ,function() {
-    _this.loadContent($(this));
+  $("select[name=day]").on("change" ,function() {
+    _this.divModule.find("div").empty();
+    _this.loadContent($(this).find(":selected"));
+    $(".buttons").remove();
   });
-}
+};
 
 $(document).ready(function() {
   var elements = {
     "divModule" : $("#specials")
   };
   var checkEventObj = new checkEvent(elements);
-  checkEventObj.init();
+  checkEventObj.bindEvents();
 });
