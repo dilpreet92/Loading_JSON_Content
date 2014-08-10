b87c033f60535ee0d863b21372c5ee1659ca0569
Dilpreet Singh <dilpreet@vinsol.com>
2014-08-10 16:17:52 +0530

    Changing the files as dicussed

--- js/specials.js
+++ js/specials.js
@@ -1,30 +1,43 @@
 function checkEvent (elements) {
   this.divModule = elements.divModule;
+  this.targetDiv = $("<div/>").appendTo(this.divModule);
+  this.availableData = [];
+}
+
+checkEvent.prototype.init = function() {
+  this.loadContent();
+  this.bindEvents();
 }
 
 checkEvent.prototype.loadContent = function(currentItem) {
-  var _this = this,
-      value = currentItem.val(),
-      targetDiv = $("<div/>").appendTo(this.divModule);
-  if (currentItem.index()) {
-    $.ajax({
-      dataType : "json",
-      url : "data/specials.json",
-      success : function(result,status,xhr) {
-        var title = $("<h2/>").text(result[value].title),
-            description = $("<p/>").text(result[value].text),
-            image = $("<img/>").attr("src", result[value].image);
-        targetDiv.append(title, description, image);    
-      }
-    });
-  }
+  var _this = this;
+  $.ajax({
+    dataType : "json",
+    url : "data/specials.json",
+    success : function(result, status, xhr) {
+      console.log(result);
+      _this.availableData = result; 
+    }
+  });
 };
+
+checkEvent.prototype.showData = function(currentItemValue) {
+  if(currentItemValue) {
+    var title = $("<h2/>").text(this.availableData[currentItemValue].title),
+        description = $("<p/>").text(this.availableData[currentItemValue].text),
+        image = $("<img/>").attr("src", this.availableData[currentItemValue].image);
+    this.targetDiv.append(title, description, image);  
+  }
+  else {
+    this.targetDiv.empty();
+  }
+}
 
 checkEvent.prototype.bindEvents = function() {
   var _this = this; 
   $("select[name=day]").on("change" ,function() {
-    _this.divModule.find("div").empty();
-    _this.loadContent($(this).find(":selected"));
+    _this.targetDiv.empty();
+    _this.showData($(this).val());
     $(".buttons").remove();
   });
 };
@@ -34,5 +47,5 @@ $(document).ready(function() {
     "divModule" : $("#specials")
   };
   var checkEventObj = new checkEvent(elements);
-  checkEventObj.bindEvents();
+  checkEventObj.init();
 });
